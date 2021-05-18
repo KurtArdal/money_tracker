020ab01a8095d14dd1cf18796799920d1f356854
Kurt Årdal <kurt@ardal.nu>
2021-05-18 16:46:15 +0200

    Moved db entry from own for loop to file for loop.

--- csv_reader.py
+++ csv_reader.py
@@ -31,18 +31,12 @@ for file in files:
         csvReader = csv.reader(csvFile, delimiter='\t')
         next(csvReader)
         for row in reversed(list(csvReader)):
-            my_list.append(row) 
-
-for list_mem in my_list:
-    if list_mem[4].find(',') != -1:
-        list_mem[4] = list_mem[4].replace(",", ".")
-
-    data = ('1',(datetime.datetime.strptime(list_mem[0], '%d.%m.%Y').strftime('%Y-%m-%d')), (datetime.datetime.strptime(list_mem[1], '%d.%m.%Y').strftime('%Y-%m-%d')), list_mem[2], list_mem[3], list_mem[4], list_mem[5], list_mem[6]) 
-    
-    mycursor.execute(ins_query, data)
-    mydb.commit()
-    
-
+            if row[4].find(',') != -1:
+                row[4] = row[4].replace(",", ".")
+            data = ('1',(datetime.datetime.strptime(row[0], '%d.%m.%Y').strftime('%Y-%m-%d')), (datetime.datetime.strptime(row[1], '%d.%m.%Y').strftime('%Y-%m-%d')), row[2], row[3], row[4], row[5], row[6]) 
+            mycursor.execute(ins_query, data)
+            mydb.commit()
+            
 mycursor.close()
 
 mydb.close()
